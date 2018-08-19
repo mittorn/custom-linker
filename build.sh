bf139a73776fa62549687ac44cd3a5852e7c7988
mittorn <mittorn@sibmail.com>
2018-08-19 21:56:32 +0000

    Fix musl build, simplify generator, glibc binaries should work on musl now

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 gcc *.c -c -I . -m32 -DANDROID_X86_LINKER -lrt -g -lm -lstdc++ -static -pthread
+ar rcs dl.a *.o
--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 cat head > lookup_gen.c
-objdump -T hl.so|less|grep \*UND| cut -f2|cut -d ' ' -f4-10| while read line; do echo void "$line"\(\)\;;done >> lookup_gen.c
+#objdump -T $1|less|grep \*UND| cut -f2|cut -d ' ' -f4-10| while read line; do echo void "$line"\(\)\;;done >> lookup_gen.c
 echo 'void *lookup_libc(const char *name) {' >> lookup_gen.c
-objdump -T hl.so|less|grep \*UND| cut -f2|cut -d ' ' -f4-10| while read line; do echo if\(\!my_strcmp\(name,\""$line"\"\)\)return "$line"\;;done >> lookup_gen.c
+objdump -T $1|less|grep \*UND| cut -f2|cut -d ' ' -f4-10| while read line; do echo extern char "$line"\;if\(\!my_strcmp\(name,\""$line"\"\)\)return "$line"\;;done >> lookup_gen.c
 cat end >> lookup_gen.c
