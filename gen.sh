8460eb71c55f1606c4a2c3897baaa70a877d7ca4
mittorn <mittorn@sibmail.com>
2018-09-01 05:36:45 +0700

    Improve wrapper generator

--- gen.sh
+++ gen.sh
@@ -3,15 +3,15 @@ cat head > lookup_gen.c
 echo 'void *lookup_libc(const char *name) {' >> lookup_gen.c
 #for f in $*;do echo $f;objdump -T $f|grep \*UND| cut -f2-100|cut -d ' ' -f4-100|grep Is; done
 
-SYMS=$(for f in $*; do objdump -T $f|grep \*UND| cut -f2-100|cut -d ' ' -f4-100; done)
-SYMS2=' '
+SYMS=$(cat syms.txt)\ $(for f in $*; do objdump -T $f|grep \*UND| cut -f2-100|cut -d ' ' -f4-100; done)
+SYMS2=" "
 for s in $SYMS
 do
 case $SYMS2 in
 *\ $s\ *) ;;
 *)
 case $s in
-_Jv_RegisterClasses|__gmon_start__|_ITM_registerTMCloneTable|_ITM_deregisterTMCloneTable) ;;
+_Jv_RegisterClasses|__gmon_start__|_ITM_registerTMCloneTable|_ITM_deregisterTMCloneTable|__libc_start_main) ;;
 curl*|__asan_*|__ubsan_*|*UTIL_*|*RuneLocale*);;
 #__*_finite|__curbrk|___brk_addr|__write|__*_chk|_strtod_internal|__strtod_internal|__finitef) ;; # musl
 __ctype_tolower|__ctype_toupper|__ctype_b);;
@@ -22,5 +22,7 @@ SYMS2="$SYMS2 $s "
 ;;
 esac
 done
+
+echo $SYMS2 > syms.txt
 
 cat end >> lookup_gen.c
